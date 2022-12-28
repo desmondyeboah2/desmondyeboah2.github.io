0c5305cfea5c22573b2d4e1bf781da8ca5989170
desmondyeboah2 <108994125+desmondyeboah2@users.noreply.github.com>
2022-12-28 17:18:59 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
 
-function shuffle(arr){
-    for(i = arr.length-1; i > 0; i--){
-    let eachItem = arr[i];
-    let k = Math.floor(Math.random() * i + 1 )
-    console.log(k)
-    arr[i] = arr[k]
-    arr[k] = eachItem
-    
-} 
-
+function shuffle(arr) {
+    for(let i = arr.length-1; i >0; i--) {
+        let lastArray = arr[i]
+        let r = Math.floor(Math.random() * i + 1);
+        //  arr[i] = arr[r];
+        //  arr[r] = lastArray;
+        [arr[i], arr[r]] = [arr[r], arr[i]]
+        document.getElementById('array-solution').innerHTML = arr;
+    }
 }
 
 
-let arr = [1, 2, 3];
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-shuffle(arr)
-console.log(arr)
-
-
-/* 
-let random = Math.floor(Math.random() * 10)
-console.log(random);
+console.log(arr);
 
 
-function random(min, max) {
-   let random =  Math.floor(Math.random() * max - min  )
-   return random;
-}
 
-console.log(random(0, 3))*/
+
+console.log(arr);
+
+document.getElementById('array-field').innerHTML = `Array = [ ${arr}]`
+
+let shuffleBtn = document.getElementById('shuffle-btn');
+
+let newArr = document.getElementById("array-solution")
+newArr.innerHTML = arr;
+
